4b6f65549d65f84f8384a7a43490a6b6a546501d
Sefa Saraç <srcsfysf@gmail.com>
2024-08-09 14:15:09 +0300

    Add files via upload

---
+++ Polymorphism/Polymorphism/BaseGeometrikSekil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class BaseGeometrikSekil
+    {
+        // Base Class için Özellikler
+        public double Genislik { get; set; }
+        public double Yukseklik { get; set; }
+
+        // Polymorphism için Virtual Metot
+        public virtual double AlanHesapla()
+        {
+            return Genislik * Yukseklik;
+        }
+    }
+
+    public class Kare : BaseGeometrikSekil;
+    public class Dikdortgen : BaseGeometrikSekil;
+    public class DikUcgen : BaseGeometrikSekil
+    {
+        // DikUcgen için AlanHesapla metodunu override ediyoruz
+        public override double AlanHesapla()
+        {
+            return (Genislik * Yukseklik) / 2;
+        }
+    }
+}
+    
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,25 @@
+﻿using Polymorphism;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Kare Nesnesi Oluşturma ve Alan Hesaplama
+        Kare kare1 = new Kare();
+        kare1.Genislik = 5;
+        kare1.Yukseklik = 5;
+        Console.WriteLine($"Kare Alanı: {kare1.AlanHesapla()}");
+
+        // Dikdörtgen Nesnesi Oluşturma ve Alan Hesaplama
+        Dikdortgen dikdortgen1 = new Dikdortgen();
+        dikdortgen1.Genislik = 4;
+        dikdortgen1.Yukseklik = 7;
+        Console.WriteLine($"Dikdörtgen Alanı: {dikdortgen1.AlanHesapla()}");
+
+        // Dik Üçgen Nesnesi Oluşturma ve Alan Hesaplama
+        DikUcgen dikUcgen1 = new DikUcgen();
+        dikUcgen1.Genislik = 3;
+        dikUcgen1.Yukseklik = 6;
+        Console.WriteLine($"Dik Üçgen Alanı: {dikUcgen1.AlanHesapla()}");
+    }
+}
